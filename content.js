c2d8a9f1b7167438b95b7c462697030a3c46d233
Daniel Page <dwpage98@gmail.com>
2023-05-23 20:36:08 +1200

    Cursor and submission updates
    - Default cursor movement behaviour ignored
    - False input added to enable submission (by enter or button)

--- content.js
+++ content.js
@@ -6,15 +6,24 @@ document.addEventListener('keydown', function(event) {
       console.log("Up key pressed");
       console.log(window.location.hostname)
       if (chat_gpt_url.includes(window.location.hostname)) {
+        event.preventDefault();
         chat_gpt_text_input = document.getElementById('prompt-textarea')
         chat_gpt_text_input.select();
-        chat_gpt_text_input.value = "Up key pressed GPT"
+        chat_gpt_text_input.value = "Up key pressed GPT";
+        chat_gpt_text_input.dispatchEvent(
+          new Event("input", { bubbles: true, cancelable: true })
+        );
       } else if (bard_url.includes(window.location.hostname)) {
+        event.preventDefault();
         bard_text_input = document.getElementById('mat-input-0');
         bard_text_input.select();
         bard_text_input.value = "Up key pressed Bard";
+        const end = bard_text_input.value.length;
+        bard_text_input.setSelectionRange(end, end);
+        bard_text_input.dispatchEvent(
+          new Event("input", { bubbles: true, cancelable: true })
+        );
       }
-      
     }
 });
 
@@ -23,13 +32,21 @@ document.addEventListener('keydown', function(event) {
     console.log("Down key pressed");
     console.log(window.location.hostname)
     if (chat_gpt_url.includes(window.location.hostname)) {
+      event.preventDefault();
       chat_gpt_text_input = document.getElementById('prompt-textarea')
       chat_gpt_text_input.select();
-      chat_gpt_text_input.value = "Down key pressed GPT"
+      chat_gpt_text_input.value = "Down key pressed GPT";
+      chat_gpt_text_input.dispatchEvent(
+        new Event("input", { bubbles: true, cancelable: true })
+      );
     } else if (bard_url.includes(window.location.hostname)) {
+      event.preventDefault();
       bard_text_input = document.getElementById('mat-input-0');
       bard_text_input.select();
       bard_text_input.value = "Down key pressed Bard";
+      bard_text_input.dispatchEvent(
+        new Event("input", { bubbles: true, cancelable: true })
+      );
     }
   }
 });
