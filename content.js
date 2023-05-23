114a4effcc1c8638e2d011ed3af5360f833bfe42
Daniel Page <dwpage98@gmail.com>
2023-05-23 17:18:37 +1200

    ChatGPT text input changed with up/down arrow keys

--- content.js
+++ content.js
@@ -1,5 +1,15 @@
+chat_gpt_text_input = document.getElementById('prompt-textarea')
+
 document.addEventListener('keydown', function(event) {
     if (event.keyCode === 38) { // 38 is the key code for the up arrow key
-      console.log("TESTING");
+      console.log("Up key pressed");
+      chat_gpt_text_input.innerText = "Up key pressed"
     }
-  });
+});
+
+document.addEventListener('keydown', function(event) {
+  if (event.keyCode === 40) { // 40 is the key code for the up arrow key
+    console.log("Down key pressed");
+    chat_gpt_text_input.innerText = "Down key pressed"
+  }
+});  
