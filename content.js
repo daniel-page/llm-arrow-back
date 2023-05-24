4454636ce5a659c0afebf3a5f8efbf65c2826ab6
Daniel Page <dwpage98@gmail.com>
2023-05-24 18:49:06 +1200

    ChatGPT prompt input history linked to up/down presses

--- content.js
+++ content.js
@@ -1,20 +1,45 @@
 const chat_gpt_url = 'https://chat.openai.com/';
 const bard_url = 'https://bard.google.com/';
 
+
+var prev_prompt_index = -1
+var arr = []
+
+function update() {
+  arr = [];
+
+  text_blocks = document.getElementsByClassName("min-h-[20px] flex flex-col items-start gap-4 whitespace-pre-wrap break-words");
+
+  for (let i = 0; i < text_blocks.length; i++) { 
+    if (text_blocks[i]["firstElementChild"] == null) {
+      arr.push(text_blocks[i].innerText)
+    }
+  }
+  arr.reverse();
+  console.log(arr);
+
+}
+
 document.addEventListener('keydown', function(event) {
     if (event.keyCode === 38) { // 38 is the key code for the up arrow key
       console.log("Up key pressed");
       console.log(window.location.hostname)
       if (chat_gpt_url.includes(window.location.hostname)) {
         event.preventDefault();
+        update();
         chat_gpt_text_input = document.getElementById('prompt-textarea')
         chat_gpt_text_input.select();
-        chat_gpt_text_input.value = "Up key pressed GPT";
+        if (prev_prompt_index + 1 < arr.length) {
+          prev_prompt_index++;
+          console.log(prev_prompt_index);
+          chat_gpt_text_input.value = arr[prev_prompt_index];
+        }
         chat_gpt_text_input.dispatchEvent(
           new Event("input", { bubbles: true, cancelable: true })
         );
       } else if (bard_url.includes(window.location.hostname)) {
         event.preventDefault();
+        update();
         bard_text_input = document.getElementById('mat-input-0');
         bard_text_input.select();
         bard_text_input.value = "Up key pressed Bard";
@@ -33,14 +58,23 @@ document.addEventListener('keydown', function(event) {
     console.log(window.location.hostname)
     if (chat_gpt_url.includes(window.location.hostname)) {
       event.preventDefault();
+      update();
       chat_gpt_text_input = document.getElementById('prompt-textarea')
       chat_gpt_text_input.select();
-      chat_gpt_text_input.value = "Down key pressed GPT";
+      if (prev_prompt_index - 1 >= 0) {
+        prev_prompt_index--;
+        console.log(prev_prompt_index);
+        chat_gpt_text_input.value = arr[prev_prompt_index];
+      } else {
+        chat_gpt_text_input.value = "";
+        prev_prompt_index = -1;
+      }
       chat_gpt_text_input.dispatchEvent(
         new Event("input", { bubbles: true, cancelable: true })
       );
     } else if (bard_url.includes(window.location.hostname)) {
       event.preventDefault();
+      update();
       bard_text_input = document.getElementById('mat-input-0');
       bard_text_input.select();
       bard_text_input.value = "Down key pressed Bard";
